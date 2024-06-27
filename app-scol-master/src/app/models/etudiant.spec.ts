acd8d73ed470d320f98f57b14665bf07560f3aa3
NathanRobertMMI <nathan.robert@edu.univ-eiffel.fr>
2024-06-27 11:52:24 +0200

    ajout app

---
+++ app-scol-master/src/app/models/etudiant.spec.ts
@@ -0,0 +1,7 @@
+import { Etudiant } from './components/etudiant';
+
+describe('Etudiant', () => {
+  it('should create an instance', () => {
+    expect(new Etudiant()).toBeTruthy();
+  });
+});
---
+++ app-scol-master/src/app/models/specialite.spec.ts
@@ -0,0 +1,7 @@
+import { Specialite } from './components/specialite';
+
+describe('Specialite', () => {
+  it('should create an instance', () => {
+    expect(new Specialite()).toBeTruthy();
+  });
+});
